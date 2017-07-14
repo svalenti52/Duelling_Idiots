b1bf6b14231a33fb84f3cff2a47d2b0a97e1422d
svalenti52 <svalenti52@yahoo.com>
2017-07-14 07:06:59 -0700

    Some documentation of blind_spider_MCS.

--- blind_spider_MCS.cpp
+++ blind_spider_MCS.cpp
@@ -2,6 +2,7 @@
  * \file blind_spider_MCS.cpp
  * \date 8-Jul-2017
  *
+ * \brief Expected number of steps to reach an absorbing state on a 2D random walk.
  *
  */
 
@@ -11,16 +12,23 @@
 
 int main(){
 
-    State state0(0, {3, 6, 7}, 0 , 2);
-    State state1(1, {2, 8}, 0, 1);
-    State state2(2, {1, 4, 7}, 0, 2);
-    State state3(3, {0}, 0, 0);
-    State state4(4, {2, 5}, 0, 1);
-    State state5(5, {4, 6}, 0, 1);
-    State state6(6, {0, 5, 7}, 0, 2);
-    State state7(7, {0, 2, 6, 8}, 0, 3);
-    State state8(8, {1, 7}, 0, 1);
+    /**
+     * Initialized fixed states having canonical IDs and fixed transition
+     * matrices. Transition selection is uniform.
+     */
+    State state0( 0, {3, 6, 7} );
+    State state1( 1, {2, 8} );
+    State state2( 2, {1, 4, 7} );
+    State state3( 3, {0} );
+    State state4( 4, {2, 5} );
+    State state5( 5, {4, 6} );
+    State state6( 6, {0, 5, 7} );
+    State state7( 7, {0, 2, 6, 8} );
+    State state8( 8, {1, 7} );
 
+    /**
+     * Initialized state vector, canonical order is important.
+     */
     std::vector<State> states{
             state0,
             state1,
@@ -33,6 +41,13 @@ int main(){
             state8
     };
 
+    /**
+     * Construct stateMatrix object with the following states:
+     *      - nr_trials
+     *      - state vector
+     *      - starting state
+     *      - absorbing state
+     */
     StateMatrix stateMatrix(10'000'000, states, 0, 1);
 
     std::cout << stateMatrix << '\n';
@@ -44,5 +59,4 @@ int main(){
     stopWatch.stop();
 
     stateMatrix.print_results();
-
 }
