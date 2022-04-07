df79e31e69f7b1b35ea6eb42cfd0c83ee688739f
svalenti52 <svalenti52@yahoo.com>
2022-04-07 15:34:35 -0700

    Make duplicate_selection.cpp a little more concise and put in some comments.

--- duplicate_selection.cpp
+++ duplicate_selection.cpp
@@ -18,27 +18,27 @@ int main() {
 
     const int ub_number_selection = 10'000;
 
-    std::vector<int> selection_tracker;
-    for ( int ix = 0; ix < ub_number_selection; ++ix )
-        selection_tracker.push_back(0);
+    std::vector<int> selection_tracker(ub_number_selection, 0);
 
     Uniform_Integral distribution(0, ub_number_selection-1, 1);
 
+    /////////////////////////////////////////////
     auto condition_met = [&selection_tracker]
             (Uniform_Integral& _distribution, double& no_dup_count, DRE& dre) -> bool {
 
         no_dup_count = 0.0;
 
-        while ( selection_tracker[_distribution.events[0]] == 0 ) {
-            no_dup_count += 1.0;
-            selection_tracker[_distribution.events[0]] = 1;
-            _distribution.reload_random_values(dre);
+        while ( selection_tracker[_distribution.events[0]] == 0 ) { // while number not already marked
+            no_dup_count += 1.0;  // increment no duplicate count
+            selection_tracker[_distribution.events[0]] = 1;  // mark it
+            _distribution.reload_random_values(dre);  // and get next random number
         }
 
-        for ( int& initial_value : selection_tracker )
-            initial_value = 0;
+        for ( int& initial_value : selection_tracker ) // unmark all for next calculation
+            initial_value = 0;  // the value in no_dup_count is accumulated to eventually determine mean
         return true;
     };
+    /////////////////////////////////////////////
 
     MonteCarloSimulation<int, double, int, std::uniform_int_distribution>
             monteCarloSimulation(
