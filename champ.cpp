38ca115272ee7f462a92ef5803d97b3013a3c3bd
svalenti52 <svalenti52@yahoo.com>
2022-05-05 06:55:38 -0700

    Update champ.cpp to handle both match drawn and winning cases.

--- champ.cpp
+++ champ.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <iostream>
-#include <random>
 #include <val/montecarlo/MonteCarloSim_beta.h>
 
 using DistributionReal = Distribution<double, double, std::uniform_real_distribution>;
@@ -11,11 +10,13 @@ using DistributionReal = Distribution<double, double, std::uniform_real_distribu
 int main()
 {
     DistributionReal D(0.0, 1.0, 6);
-
-auto condition_met{[](DistributionReal& gameset, double& matchres, DRE& dre)
+    const int nr_trials = 1'000'000;
+    const double prob_win = 0.6;
+    const double prob_draw = 0.1;
+    const int common_seed = 7;
+    //===========================================
+    auto condition_met_draw{[prob_win, prob_draw](DistributionReal& gameset, double& matchres, DRE& dre)
         {
-            const double prob_win = 0.8;
-            const double prob_draw = 0.1;
             int matchresult = 0;
             for (double x : gameset.events)
             {
@@ -28,14 +29,49 @@ auto condition_met{[](DistributionReal& gameset, double& matchres, DRE& dre)
             if (matchresult == 6) return true;
             return false;
         }};
+    //===========================================
 
-    MonteCarloSimulation<double, double, double, std::uniform_real_distribution> monteCarloSimulation
-            (1'000'000,
-             7,
-             condition_met,
+    //===========================================
+    auto condition_met_win{[prob_win, prob_draw](DistributionReal& gameset, double& matchres, DRE& dre)
+                            {
+                                int matchresult = 0;
+                                for (double x : gameset.events)
+                                {
+                                    if (x < prob_win)
+                                        matchresult += 2;
+                                    else if (x < prob_win+prob_draw)
+                                        ++matchresult;
+                                }
+                                gameset.reload_random_values(dre);
+                                if (matchresult > 6) return true;
+                                return false;
+                            }};
+    //===========================================
+
+
+    std::cout << "\nBased on Probabilities:  Champ Wins = " << prob_win << ",  Drawn Match = " << prob_draw << "\n\n";
+
+    MonteCarloSimulation<double, double, double, std::uniform_real_distribution> monteCarloSimulation_draw
+            (nr_trials,
+             common_seed,
+             condition_met_draw,
              D);
 
-    monteCarloSimulation.run();
+    MonteCarloSimulation<double, double, double, std::uniform_real_distribution> monteCarloSimulation_win
+            (nr_trials,
+             common_seed,
+             condition_met_win,
+             D);
 
-    monteCarloSimulation.print_result();
+    monteCarloSimulation_win.run();
+
+    monteCarloSimulation_win.change_message("Probability Champ wins match = ");
+
+    monteCarloSimulation_win.print_result();
+
+    monteCarloSimulation_draw.run();
+
+    monteCarloSimulation_draw.change_message("Probability of drawn match = ");
+
+    monteCarloSimulation_draw.print_result();
 }
